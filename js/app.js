8dd4b9ea5a79bab2c3fdbfca2cc925a8f97578d6
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-04-11 13:14:35 -0500

    Se cambio un poco la identacion del codigo en el archivo app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 //4152313380623160
 var input;
 function isValidCard(input){
@@ -14,69 +13,69 @@ var sumTotal=0;
 var cardValid;
 var cardInvalid;
 do{
-do{
-input= prompt("INGRESA TU NUMERO DE TARJETA DE CREDITO");
+   do{
+       input= prompt("INGRESA TU NUMERO DE TARJETA DE CREDITO");
 
-if(input.length != 16 || input === ''){
- alert( 'RECUERDA DEBES INGRESAR 16 DIGITOS ');
-}else{
+    if(input.length != 16 || input === ''){
+         alert( 'RECUERDA DEBES INGRESAR 16 DIGITOS ');
+      }else{
 
- for(var i=0; i< input.length;i++){
-    inputAscii=input.charCodeAt(i);
-    if(inputAscii>=48 && inputAscii<=57){
-      arrayAscii.push(inputAscii);
+         for(var i=0; i< input.length;i++){
+            inputAscii=input.charCodeAt(i);
+            if(inputAscii>=48 && inputAscii<=57){
+              arrayAscii.push(inputAscii);
 
-     }else{
-       alert('INGRESASTE UN SIMBOLO INVALIDO...NOTA:INGRESA DIGITOS DEL 0 AL 9 ');
-     break;
-    }//cierra else
+             }else{
+               alert('INGRESASTE UN SIMBOLO INVALIDO...NOTA:INGRESA DIGITOS DEL 0 AL 9 ');
+             break;
+            }//cierra else
 
-  }//cierra for en i
+          }//cierra for en i
 
- reverseArrayAscii=arrayAscii.reverse(); // se invierte el arreglo obtenido en el for
+         reverseArrayAscii=arrayAscii.reverse(); // se invierte el arreglo obtenido en el for
 
- for(var j=0;j<reverseArrayAscii.length; j ++){
-    str= String.fromCharCode(reverseArrayAscii[j]);//se convierte cada valor en Ascii a texto convencional.
+         for(var j=0;j<reverseArrayAscii.length; j ++){
+            str= String.fromCharCode(reverseArrayAscii[j]);//se convierte cada valor en Ascii a texto convencional.
 
-    arrayInt.push(parseInt(str));//se convierte la String a un Entero y se Agrega a un arreglo
+            arrayInt.push(parseInt(str));//se convierte la String a un Entero y se Agrega a un arreglo
 
-  }//cierre del for en j
- //console.log(reveArrayAscii);
+          }//cierre del for en j
+         //console.log(reveArrayAscii);
 
-for(var k=0;k<arrayInt.length;k++){
-   if((k+1)%2===0){
-       multyPar=arrayInt[k]*2;
+        for(var k=0;k<arrayInt.length;k++){
+           if((k+1)%2===0){
+               multyPar=arrayInt[k]*2;
 
-      if(multyPar>=10){
-        sumaDigits=(multyPar-10)+1;// Se tiene que convertir el valor de 10 a un solo digito por esta razon se le resta la base de 10 a todos y se suma el numero 1 ya que este es el que se encuentra como base en cada numero que es mayor a 10 ya que la multiplicacion no puede dar mas de 18
+              if(multyPar>=10){
+                sumaDigits=(multyPar-10)+1;// Se tiene que convertir el valor de 10 a un solo digito por esta razon se le resta la base de 10 a todos y se suma el numero 1 ya que este es el que se encuentra como base en cada numero que es mayor a 10 ya que la multiplicacion no puede dar mas de 18
 
-         arrayInt[k]=sumaDigits;//manda el resultado de la suma de los digitos  a la posicion correspondiente del arreglo
+                 arrayInt[k]=sumaDigits;//manda el resultado de la suma de los digitos  a la posicion correspondiente del arreglo
 
-     }else {//if que se encuentra anidado en el if que esta dentro del for en k.
-         arrayInt[k]=multyPar;//se asigna directamente el valor multiplicado por 2 cuyo resultado no es mayor a 10.
-     }//cierra el else de multyParResult.
+             }else {//if que se encuentra anidado en el if que esta dentro del for en k.
+                 arrayInt[k]=multyPar;//se asigna directamente el valor multiplicado por 2 cuyo resultado no es mayor a 10.
+             }//cierra el else de multyParResult.
 
-  } //cierra el if dentro del for en k
+          } //cierra el if dentro del for en k
 
-  sumTotal +=arrayInt[k]; // suma cada valor de cada una de las posiciones del arreglo en k
-  //console.log(sumTotal);
+          sumTotal +=arrayInt[k]; // suma cada valor de cada una de las posiciones del arreglo en k
+          //console.log(sumTotal);
 
-}//cierra el for en k
+       }//cierra el for en k
 
-if(sumTotal%10===0){
-//console.log(sumTotal);
+      if(sumTotal%10===0){
+        //console.log(sumTotal);
 
- cardValid=alert("EL NUMERO DE TU TARJETA DE CREDITO ES VALIDA");
- return cardValid;
-}else{//Cierra el if que se encuentra dentro del for en j
-// console.log(sumTotal);
-cardInvalid=alert( " EL NUMERO DE TU TARJETA ES INVALIDO ");
-return cardInvalid;
-}//cierra el else de la suma total
+         cardValid=alert("EL NUMERO DE TU TARJETA DE CREDITO ES VALIDA");
+         return cardValid;
+       }else{//Cierra el if que se encuentra dentro del for en j
+     // console.log(sumTotal);
+        cardInvalid=alert( " EL NUMERO DE TU TARJETA ES INVALIDO ");
+        return cardInvalid;
+       }//cierra el else de la suma total
 
-}//cierre del else principal donde se validan que se ingresen digitos
+     }//cierre del else principal donde se validan que se ingresen digitos
 
-}while(inputAscii<48 || inputAscii>57);
+   }while(inputAscii<48 || inputAscii>57);
 
 }while(input.length!=16 || input === "");
 
